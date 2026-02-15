d628f069fd6a5d3f0d00458be8bcef49a955afdd
Archita242007 <architasablok2652@gmail.com>
2026-02-15 15:39:45 +0530

    q12 done

---
+++ PATTERNS/q12.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class q12 {
+      public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int c=0;
+        System.out.print("enter a number: ");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+           {c++;
+            System.out.print(c+" ");
+        }System.out.println();
+    }sc.close();
+}}
+
+
