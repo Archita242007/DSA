e460c956c93ca0ef04195bc2a0f13971f139d792
Archita242007 <architasablok2652@gmail.com>
2026-02-17 20:27:07 +0530

    done

---
+++ basic maths/palindrome.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class palindrome {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number: ");
+        int n=sc.nextInt();
+         int num=n;int r=0;int q;
+    while(num>0){
+        q=num%10;
+        r=10*r +q;
+        num/=10;
+    }
+    if(r==n){
+        System.out.println("true");
+    }else System.out.println("false");
+        sc.close();
+    }
+}
