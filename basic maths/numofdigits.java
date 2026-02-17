f114521a1d6e5916be8213bd535c42f40d1615af
Archita242007 <architasablok2652@gmail.com>
2026-02-17 19:35:31 +0530

    done

--- basic maths/numofdigits.java
+++ basic maths/numofdigits.java
@@ -1,15 +1,16 @@
+
 import java.util.Scanner;
 public class numofdigits{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         System.out.println("enter a number: ");
-        int n=sc.nextInt();
-        int c=0;
-        while(n>0){
-             n=n/10;
-        c=c+1;
-        }
-        System.out.println(c);
+        double n=sc.nextDouble();
+        // while(n>0){
+        //      n=n/10;
+        //  c=c+1;
+n = Math.abs(n);
+int count = (int)(Math.log10(n) + 1);
+System.out.println("Number of digits: " + count);
         sc.close();
     }
 }
